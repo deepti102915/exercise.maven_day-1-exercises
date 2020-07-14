79839e32179c5fe98e18749fb0dd0bf5a6b3a832
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-14 19:30:03 -0400

    day-1-exercises completed

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -4,27 +4,42 @@ package com.github.perscholas;
  * Created by leon on 7/14/2020.
  */
 public class GradeEvaluator {
-    public Boolean isGradeA(Double score) {
-        return null;
+    public Boolean isGradeA(double score) {
+        return (score >= 90 && score <= 100);
     }
 
-    public Boolean isGradeB(Double score) {
-        return null;
+    public Boolean isGradeB(double score) {
+        return (score >= 80 && score <= 89);
     }
 
-    public Boolean isGradeC(Double score) {
-        return null;
+    public Boolean isGradeC(double score) {
+        return (score >= 70 && score <= 79);
     }
 
-    public Boolean isGradeD(Double score) {
-        return null;
+    public Boolean isGradeD(double score) {
+        return (score >= 65 && score <= 69);
     }
 
-    public Boolean isGradeF(Double score) {
-        return null;
+    public Boolean isGradeF(double score) {
+        return (score <= 64);
     }
 
     public Character getGrade(Double score) {
-        return null;
+
+        if (isGradeA(score)) {
+            return 'A';
+        } else if (isGradeB(score)) {
+            return 'B';
+        } else if (isGradeC(score)) {
+            return 'C';
+        } else if (isGradeD(score)) {
+            return 'D';
+        }
+        else if (isGradeF(score)) {
+            return 'F';
+        }
+        else return 'F';
+
     }
 }
+
